009f9d2892a68280c3abe86a34b736b2de7e6eec
josh-sj <joshua.s.jiang@hotmail.com>
2016-10-31 16:30:31 -0400

    more data to display

--- SalesTool/src/ca/demo/salestool/SalesData.java
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -2,11 +2,11 @@ package ca.demo.salestool;
 
 public class SalesData {
 
-	int data[] ={0,4,42};
+	int data[] ={0,4,42,123, 325, 998}; //a conflict?
 	
 	public void display(){
 		System.out.println();
-		System.out.println("Data: ");
+		System.out.println("Data are: ");
 		System.out.println("=================================");
 		for(int i=0; i< data.length;i++){
 			System.out.println("data : "+data[i]);
