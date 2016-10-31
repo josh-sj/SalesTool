098d57364fef3e5df91748752ca882e67a3ed90b
josh_sj <joshua.s.jiang@hotmail.com>
2016-10-31 02:42:47 -0400

    Initial commit of sales tool.

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,16 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+
+		displayGreeting();
+		data.display();
+	}
+	
+public static void displayGreeting(){
+	System.out.println("HELLO HAPPY SALES PEOPLE!");
+	System.out.println("THIS APP SHARES SALES DATA.");
+}
+}
