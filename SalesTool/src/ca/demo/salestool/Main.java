d4f33fcfd45b09a31adfdac3389cf98e2fadad20
josh-sj <joshua.s.jiang@hotmail.com>
2016-10-31 16:07:08 -0400

    fixed greeting to upper lower cases.

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -1,5 +1,7 @@
 package ca.demo.salestool;
 
+import java.io.InputStream;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,8 +12,11 @@ public class Main {
 	}
 	
 public static void displayGreeting(){
-	System.out.println("HELLO HAPPY SALES PEOPLE!");
-	System.out.println("THIS APP SHARES SALES DATA.");
+	//String GREET1 = "HELLO HAPPY SALES PEOPLE!";
+	
+	 
+	System.out.println("Hello Happy Sales People");
+	System.out.println("This app shares sales data.");
 	
 	System.out.println("Test 1");
 	System.out.println("Test 2 added from github");
